0dbae7090583dbd203659576d9f335ae8d4ecee9
Alexander Kuznetsov <alex@kuznetcoff.ru>
2025-02-26 20:44:35 +0400

    Merged SetPage() and GetCheer() functions into a single class EBook

--- src/ebook.cpp
+++ src/ebook.cpp
@@ -3,57 +3,57 @@
 #include <iostream>
 #include <vector>
 
-const size_t max_readers = 100'000;
-const size_t max_pages = 1'000;
-
-struct Context {
+class EBook {
+private:
     std::vector<int> reader;
     std::vector<int> page;
     int num_of_readers;
 
-    Context(const int max_readers, const int max_pages)
+public:
+    static const size_t max_readers = 100'000;
+    static const size_t max_pages = 1'000;
+
+    EBook()
         : reader(max_readers + 1u, -1)
         , page(max_pages + 1u, 0)
         , num_of_readers(0)
         {}
+
+    void SetPage(const int user_id, const int page_no) {
+        const int reader_prev_page = reader[user_id];
+
+        // Check if it is a first update for the reader
+        if(reader_prev_page < 0) {
+            ++num_of_readers;
+        }
+
+        // Update read pages counters
+        for(int i = reader_prev_page + 1; i <= page_no; ++i) {
+            ++page[i];
+        }
+
+        // Save reader's last page
+        reader[user_id] = page_no;
+    }
+
+    double GetCheer(const int user_id) {
+        // Check if there are no readers yet or the user has not started to read yet
+        if(num_of_readers == 0 || reader[user_id] < 0) {
+            return 0.0;
+        }
+
+        // Check if the reader is the only reader
+        if(num_of_readers == 1) {
+            return 1.0;
+        }
+
+        return static_cast<double>(num_of_readers - page[reader[user_id]])
+            / static_cast<double>(num_of_readers - 1);
+    }
 };
 
-void SetPage(Context& ctx, const int user_id, const int page_no) {
-    const int reader_prev_page = ctx.reader[user_id];
-
-    // Check if it is a first update for the reader
-    if(reader_prev_page < 0) {
-        ++ctx.num_of_readers;
-    }
-
-    // Update read pages counters
-    for(int i = reader_prev_page + 1; i <= page_no; ++i) {
-        ++ctx.page[i];
-    }
-
-    // Save reader's last page
-    ctx.reader[user_id] = page_no;
-}
-
-double GetCheer(Context& ctx, const int user_id) {
-    if(ctx.num_of_readers == 0 || ctx.reader[user_id] < 0) {
-        // There are no readers yet or the user has not started to read yet
-        return 0.0;
-    }
-
-    if(ctx.num_of_readers == 1) {
-        // The reader is the only reader
-        return 1.0;
-    }
-
-    const int users_no_read_till_page = ctx.page[ctx.reader[user_id]];
-
-    return static_cast<double>(ctx.num_of_readers - users_no_read_till_page)
-        / static_cast<double>(ctx.num_of_readers - 1);
-}
-
 void ProcessCommands(std::istream& in) {
-    Context ctx(max_readers, max_pages);
+    EBook ebook;
 
     int num_commands = 0;
     in >> num_commands;
@@ -68,10 +68,10 @@ void ProcessCommands(std::istream& in) {
         if(command == "READ") {
             in >> page_no;
 
-            SetPage(ctx, user_id, page_no);
+            ebook.SetPage(user_id, page_no);
         }
         else if(command == "CHEER") {
-            std::cout << std::setprecision(6) << GetCheer(ctx, user_id) << std::endl;
+            std::cout << std::setprecision(6) << ebook.GetCheer(user_id) << std::endl;
         }
         else {
             std::cout << "ERROR: Unknown command [" << command << "]" << std::endl;
